76163b95db0aa8f4fb97599a9210c9aa0ec75eba
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-05-29 16:18:42 +0530

    manga page works correctly

--- src/Manga_page/mangacard.jsx
+++ src/Manga_page/mangacard.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Mangadetails from './mangadetails'
+import React from 'react';
+import Mangadetails from './mangadetails';
 
-export default function Mangacard({Manga}) {
+export default function Mangacard({ manga }) {
   return (
     <div>
       <a href="#" className="block">
-  {/* <img
-    alt="Manga Cover"
-    src={Manga.images.jpg.image_url}
-    className="h-64 w-full object-cover sm:h-80 lg:h-96 rounded-md shadow-md"
-  /> */}
+        {manga.images && manga.images.jpg && (
+          <img
+            alt="Manga Cover"
+            src={manga.images.jpg.image_url}
+            className="h-64 w-full object-cover sm:h-80 lg:h-96 rounded-md shadow-md"
+          />
+        )}
 
-  <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">{Manga.title}</h3>
+        {manga.title && (
+          <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+            {manga.title}
+          </h3>
+        )}
 
-  <Mangadetails manga={Manga}/>
-</a>
+        <Mangadetails manga={manga} />
+      </a>
     </div>
-  )
+  );
 }
--- src/Manga_page/mangadetails.jsx
+++ src/Manga_page/mangadetails.jsx
@@ -1,57 +1,57 @@
-import React from 'react'
+import React from 'react';
 
-export default function Mangadetails({manga}) {
+export default function Mangadetails({ manga }) {
   return (
     <div>
       <div className="flow-root">
-  <dl className="-my-3 divide-y divide-gray-100 text-sm">
-    <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt className="font-medium text-gray-900">Title</dt>
-      <dd className="text-gray-700 sm:col-span-2">{manga.title}</dd>
-    </div>
-
-    <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt className="font-medium text-gray-900">Authors</dt>
-      <dd className="text-gray-700 sm:col-span-2">
-        {manga.data.authors.map((item, index) => (
-          <div key={index} >
-            {item.name}
+        <dl className="-my-3 divide-y divide-gray-100 text-sm">
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Title</dt>
+            <dd className="text-gray-700 sm:col-span-2">{manga.title}</dd>
           </div>
-        ))}
-        </dd>
-    </div>
 
-    <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt className="font-medium text-gray-900">Status</dt>
-      <dd className="text-gray-700 sm:col-span-2">{manga.status}</dd>
-    </div>
-
-    <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt className="font-medium text-gray-900">Published</dt>
-      <dd className="text-gray-700 sm:col-span-2">{manga.published.string}</dd>
-    </div>
-
-    <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt className="font-medium text-gray-900">Synopsis</dt>
-      <dd className="text-gray-700 sm:col-span-2">{manga.synopsis}</dd>
-    </div>
-
-    <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt className="font-medium text-gray-900">Genres</dt>
-      <dd className="text-gray-700 sm:col-span-2">
-      {manga.genres.map((item, index) => (
-          <div key={index} >
-            <span className="mt-1 whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-sm text-blue-700">
-            {item.name}
-            </span>
-            
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Authors</dt>
+            <dd className="text-gray-700 sm:col-span-2">
+              {manga.authors && manga.authors.map((item, index) => (
+                <div key={index}>
+                  {item.name}
+                </div>
+              ))}
+            </dd>
           </div>
-        ))}
-      </dd>
-    </div>
 
-  </dl>
-</div>
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Status</dt>
+            <dd className="text-gray-700 sm:col-span-2">{manga.status}</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Published</dt>
+            <dd className="text-gray-700 sm:col-span-2">
+              {manga.published && manga.published.string}
+            </dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Synopsis</dt>
+            <dd className="text-gray-700 sm:col-span-2">{manga.synopsis}</dd>
+          </div>
+
+          <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+            <dt className="font-medium text-gray-900">Genres</dt>
+            <dd className="text-gray-700 sm:col-span-2">
+              {manga.genres && manga.genres.map((item, index) => (
+                <div key={index}>
+                  <span className="mt-1 whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-sm text-blue-700">
+                    {item.name}
+                  </span>
+                </div>
+              ))}
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
-  )
+  );
 }
