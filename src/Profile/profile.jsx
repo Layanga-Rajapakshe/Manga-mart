2e5d2c25a89903a1ca5e235d712ea16df5dfb684
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-06-17 10:44:41 +0530

    dashboard updated

--- src/Profile/profile.jsx
+++ src/Profile/profile.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Sidebar from './profile_sidebar'
 import { useSelector } from 'react-redux'
+import Profile_statistics_card from './profile_statistics_card'
+import Profile_users_table from './profile_users_table';
+import Profile_recent_orders from './profile_recent_orders';
 
 function profile() {
   const { userInfo } = useSelector((state) => state.auth);
@@ -11,6 +14,21 @@ function profile() {
       {/* Main content */}
       <div className="flex-1 bg-gray-200">
         {/* Your main content goes here */}
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
+          <Profile_statistics_card />
+          <Profile_statistics_card />
+          <Profile_statistics_card />
+          <Profile_statistics_card />
+        </div>
+
+        <div class="grid grid-cols-1 lg:grid-cols-2 p-4 gap-4">
+    
+          {/* users table */}
+          <Profile_users_table />
+
+          {/* orders table */}
+          <Profile_recent_orders />
+        </div>
       </div>
     </div>
   )
