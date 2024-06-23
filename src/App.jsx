ca8f4fc75a1c12988856ce0d674299a13d7a15ef
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-06-23 12:57:33 +0530

    routes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,59 @@
-import React, { useState,useEffect } from 'react';
-import HeroSection from './Landing_page/hero';
-import Navbar from './Landing_page/navbar';
-import Footer from './Landing_page/footer';
-import Card from './Home_page/card';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Homepage from './Home_page/homepage';
-import Loginpage from './Login_page/loginpage';
+import Footer from './Landing_page/footer';
+import HeroSection from './Landing_page/hero';
 import Aboutpage from './About_page/aboutpage';
+import Signuppage from './Login_page/signuppage';
+import Loginpage from './Login_page/loginpage';
+import Product from './Product_page/product';
+import { Toaster } from 'react-hot-toast';
+import Settings from './Profile/settings';
+import Favourites from './Profile/favourites';
+import Cart from './Cart/cart';
+import PrivateRoute from './Components/PrivateRoute';
+import Shipping from './Orders/shipping';
+import PlaceOrder from './Orders/placeorder';
+import Order from './Orders/order';
+import Hero from './Video_hero_page/hero';
+import Navbar from './Navbar/navbar';
+import Profile from './Profile/profile';
+import Orders from './Profile/profile_orders';
+import Genrepage from './Genres/genrepage';
+import Search from './Search_page.jsx/search';
 
-function App() {
+const App = () => {
   return (
     <div>
       <Navbar />
-      <HeroSection />
+      <div className="main-content">
+        <Routes>
+          <Route path="/test" element={<HeroSection />} />
+          <Route path="/" element={<Hero />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/about" element={<Aboutpage />} />
+          <Route path="/signup" element={<Signuppage />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/genre/:name/:genre" element={<Genrepage />} />
+          <Route path="/search/:query" element={<Search />} />
+
+          <Route path="" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/shipping" element={<Shipping />} />
+            <Route path="/placeorder" element={<PlaceOrder />} />
+            <Route path="/order/:id" element={<Order />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/wishlist" element={<Favourites />} />
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+        </Routes>
+        <Toaster />
+      </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
