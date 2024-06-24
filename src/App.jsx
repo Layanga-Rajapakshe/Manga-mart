ded43f8a9a27acaab37f03cf4f06b24f808c1428
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-06-24 23:05:21 +0530

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,63 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Homepage from './Home_page/homepage';
-import Footer from './Landing_page/footer';
-import HeroSection from './Landing_page/hero';
-import Aboutpage from './About_page/aboutpage';
-import Signuppage from './Login_page/signuppage';
-import Loginpage from './Login_page/loginpage';
-import Product from './Product_page/product';
 import { Toaster } from 'react-hot-toast';
-import Settings from './Profile/settings';
-import Favourites from './Profile/favourites';
-import Cart from './Cart/cart';
-import PrivateRoute from './Components/PrivateRoute';
-import Shipping from './Orders/shipping';
-import PlaceOrder from './Orders/placeorder';
-import Order from './Orders/order';
-import Hero from './Video_hero_page/hero';
 import Navbar from './Navbar/navbar';
-import Profile from './Profile/profile';
-import Orders from './Profile/profile_orders';
-import Genrepage from './Genres/genrepage';
-import Search from './Search_page.jsx/search';
-import Errorpage from './404 page/errorpage';
+import Footer from './Landing_page/footer';
+import Loading from './Components/Loader';
+import PrivateRoute from './Components/PrivateRoute';
+
+const Homepage = lazy(() => import('./Home_page/homepage'));
+const HeroSection = lazy(() => import('./Landing_page/hero'));
+const Aboutpage = lazy(() => import('./About_page/aboutpage'));
+const Signuppage = lazy(() => import('./Login_page/signuppage'));
+const Loginpage = lazy(() => import('./Login_page/loginpage'));
+const Product = lazy(() => import('./Product_page/product'));
+const Settings = lazy(() => import('./Profile/settings'));
+const Favourites = lazy(() => import('./Profile/favourites'));
+const Cart = lazy(() => import('./Cart/cart'));
+const Shipping = lazy(() => import('./Orders/shipping'));
+const PlaceOrder = lazy(() => import('./Orders/placeorder'));
+const Order = lazy(() => import('./Orders/order'));
+const Hero = lazy(() => import('./Video_hero_page/hero'));
+const Profile = lazy(() => import('./Profile/profile'));
+const Orders = lazy(() => import('./Profile/profile_orders'));
+const Genrepage = lazy(() => import('./Genres/genrepage'));
+const Search = lazy(() => import('./Search_page.jsx/search'));
+const Errorpage = lazy(() => import('./404 page/errorpage'));
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <div className="main-content">
-        <Routes>
-          <Route path="/test" element={<HeroSection />} />
-          <Route path="/" element={<Hero />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/about" element={<Aboutpage />} />
-          <Route path="/signup" element={<Signuppage />} />
-          <Route path="/login" element={<Loginpage />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/genre/:name/:genre" element={<Genrepage />} />
-          <Route path="/search/:query" element={<Search />} />
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/test" element={<HeroSection />} />
+            <Route path="/" element={<Hero />} />
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/about" element={<Aboutpage />} />
+            <Route path="/signup" element={<Signuppage />} />
+            <Route path="/login" element={<Loginpage />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/genre/:name/:genre" element={<Genrepage />} />
+            <Route path="/search/:query" element={<Search />} />
 
-          {/* Private routes */}
-          <Route element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/shipping" element={<Shipping />} />
-            <Route path="/placeorder" element={<PlaceOrder />} />
-            <Route path="/order/:id" element={<Order />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/wishlist" element={<Favourites />} />
-            <Route path="/cart" element={<Cart />} />
-          </Route>
+            {/* Private routes */}
+            <Route element={<PrivateRoute />}>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/shipping" element={<Shipping />} />
+              <Route path="/placeorder" element={<PlaceOrder />} />
+              <Route path="/order/:id" element={<Order />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/wishlist" element={<Favourites />} />
+              <Route path="/cart" element={<Cart />} />
+            </Route>
 
-          {/* Catch-all route for 404 Not Found */}
-          <Route path="*" element={<Errorpage />} />
-        </Routes>
+            {/* Catch-all route for 404 Not Found */}
+            <Route path="*" element={<Errorpage />} />
+          </Routes>
+        </Suspense>
         <Toaster />
       </div>
       <Footer />
