e30a6b96dc4e6aef76bb1b11e37ed5fb8128bbef
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-06-24 23:17:40 +0530

    updated

--- src/404 page/errorpage.jsx
+++ src/404 page/errorpage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function errorpage() {
   return (
-    <div>
+    <div className='pt-12'>
       <div className="grid h-screen place-content-center bg-white px-4">
         <h1 className="uppercase tracking-widest text-gray-500">404 | Not Found</h1>
         <Link to="/" className="text-blue-600 hover:underline">
